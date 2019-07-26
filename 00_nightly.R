d9a547a8fa2e31d014f36cbc443825c1a9f99e0b
Gayen <igayen@RBI1.rbi.org.in>
2019-07-26 10:27:48 +0530

    V0.1.3 - testing NEWS.md

---
+++ 00_nightly.R
@@ -0,0 +1,10 @@
+
+require(usethis)
+# usethis::use_devtools()
+
+use_git_config(user.name = "Indranil Gayen", user.email = "nil.gayen@gmail.com")
+#use_git_config(user.name = "Indranil Gayen", user.email = "nil.gayen@gmail.com")
+git_sitrep()
+
+# followed this 
+# https://happygitwithr.com/credential-caching.html
